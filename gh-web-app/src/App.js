84d20c9e57df74638ad24f040176bb5535278d05
Khoi <khoile@stanford.edu>
2018-10-14 00:50:50 -0500

    firebase interaction is pretty robust

--- gh-web-app/src/App.js
+++ gh-web-app/src/App.js
@@ -16,8 +16,12 @@ class App extends Component {
       dataFromFirebase: [],
       dataFieldsFromFirebase: [],
       itemNames: [],
-      selectedColumn: 0
+      selectedColumn: 0,
+      selectedField: "",
+      questionPreviewText: ""
     };
+    this.addNewEntry = this.addNewEntry.bind(this);
+    this.setColumn = this.setColumn.bind(this);
   }
 
   setProvider(providerList) {
@@ -31,8 +35,20 @@ class App extends Component {
 
   setColumn(col)
   {
-    this.setState({
-      selectedColumn: col
+    console.log("COLUMN SELECTED: " + col);
+    var colHeaders = this.state.dataFromFirebase[this.state.itemNames.indexOf("TRACKED-DATA")];
+    var lookUpField = colHeaders[col];
+    var questionText = "";
+    const infoRef = firebase.database().ref("web-app").child("provider-lists").child(this.state.currentProviderList).child("DATA-TO-QUESTIONS");
+    infoRef.once('value', (snapshot) => {
+      let items = snapshot.val();
+      questionText = items[lookUpField];
+      this.setState({
+        selectedColumn: col,
+        questionPreviewText: questionText,
+        selectedField: lookUpField
+      });
+      console.log(questionText);
     });
   }
 
@@ -49,22 +65,21 @@ class App extends Component {
       let newData = [];
       let dataFields = [];
       let itemNamesTemp = [];
-      for (let item in items) {
-        itemNamesTemp.push(item);
 
-        if (item === "TRACKED-DATA")
+      dataFields = items["TRACKED-DATA"];
+      var dataHeaders = [];
+      for(let field in dataFields)
+      {
+        dataHeaders.push(dataFields[field]);
+      }
+      dataFields = dataHeaders;
+      newData.push(dataHeaders);
+      itemNamesTemp.push("TRACKED-DATA");
+
+      for (let item in items) {
+        if (item !== "TRACKED-DATA" && item !== "DATA-TO-QUESTIONS")
         {
-          dataFields = items[item];
-          var dataHeaders = [];
-          for(let field in dataFields)
-          {
-            dataHeaders.push(dataFields[field]);
-          }
-          dataFields = dataHeaders;
-          newData.push(dataHeaders);
-        }
-        else if (item !== "TRACKED-DATA" && item !== "DATA-TO-QUESTIONS")
-        {
+          itemNamesTemp.push(item);
           var newItem = {
             id: item
           };
@@ -93,20 +108,27 @@ class App extends Component {
 
   addNewEntry()
   {
-    console.log("NEW ENTRY" + this.state.currentProviderList);
+    console.log("NEW ENTRY " + this.state.currentProviderList);
     var newData = this.state.itemNames.slice();
     const infoRef = firebase.database().ref("web-app").child("provider-lists").child(this.state.currentProviderList);
     var newItemName = infoRef.push().key;
     var newRef = infoRef.child(newItemName);
-    newRef.update({
-      "ADDRESS": "LOL"
-    });
 
+    var newItem = {};
+    console.log(this.state.dataFieldsFromFirebase);
+    var colHeaders = this.state.dataFromFirebase[this.state.itemNames.indexOf("TRACKED-DATA")];
+    console.log(colHeaders);
+    for (let field in colHeaders)
+    {
+      newItem[colHeaders[field]] = " ";
+    }
+    newRef.update(newItem);
 
     newData.push(newItemName);
     this.setState({
       itemNames: newData
     });
+    this.pullFromFirebase(this.state.currentProviderList);
   }
 
   showDashboard() {
@@ -124,7 +146,8 @@ class App extends Component {
           dataFieldsFromFirebase={this.state.dataFieldsFromFirebase}
           dataFromFirebase={this.state.dataFromFirebase}
           dataItemNames={this.state.itemNames}
-          addNewEntry={() => this.addNewEntry()}
+          addNewEntry={this.addNewEntry}
+          setColumn={this.setColumn}
           providerList={providerListName} goBack={() => this.showDashboard()} />
       );
   }
@@ -139,8 +162,11 @@ class App extends Component {
 
   renderQuestionPreview()
   {
+    console.log(this.state.questionPreviewText);
       return(
-            <QuestionPreview />
+            <QuestionPreview
+              questionText={this.state.questionPreviewText}
+              selectedField={this.state.selectedField} />
       );
   }
 
@@ -150,7 +176,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h3> SLaftey Net </h3>
+          <h3> Samaritan </h3>
         </div>
         <div className="MainContent">
           <div className="Dashboard-Container">
@@ -170,11 +196,32 @@ class App extends Component {
 
 class QuestionPreview extends Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      questionText: props.questionText
+    };
+  }
+
+  handleChange(e)
+  {
+    this.setState({questionText: e.target.value});
+  }
+
   render()
   {
     return (
       <div>
         <h2> Question preview </h2>
+        <p> Your current selected field is: </p>
+        <p className="questionText"> {this.props.selectedField} </p>
+        <br/>
+        <p> To help your users filter for useful results, Samaritan will ask: </p>
+        <p className="questionText"> {this.props.questionText} </p>
+        <p> If you would like to change this question, type a new question in the box below </p>
+        <input type='text' value={this.state.questionText} onChange={this.handleChange} />
+        <button> See question tree flow </button>
       </div>
     );
   }
@@ -190,8 +237,10 @@ class DashboardView extends Component
           <h3> Provider Info </h3>
              <button className="ProviderSelect" onClick={() => this.props.onSetProvider("healthcare-providers")}> Healthcare </button>
              <button className="ProviderSelect" onClick={() => this.props.onSetProvider("childcare-providers")}> Child Care </button>
-             <button className="ProviderSelect"> Legal Services </button>
-             <button className="ProviderSelect"> Employment Services </button>
+             <button className="ProviderSelect" onClick={() => this.props.onSetProvider("legalservice-providers")}> Legal Services </button>
+             <button className="ProviderSelect" onClick={() => this.props.onSetProvider("employment-providers")}> Employment Services </button>
+           <h3> Manage Chat Flow </h3>
+              <button className="ProviderSelect"> Edit Flow </button>
         </div>
       </div>
     );
@@ -218,7 +267,8 @@ class ProviderManager extends Component {
           dataFieldsFromFirebase={this.props.dataFieldsFromFirebase}
           dataFromFirebase={this.props.dataFromFirebase}
           dataItemNames={this.props.dataItemNames}
-          addNewEntry={() => this.props.addNewEntry()}
+          addNewEntry={this.props.addNewEntry}
+          setColumn={this.props.setColumn}
           reloadFromFirebase={this.props.reloadFromFirebase} />
       </div>
     );
@@ -252,19 +302,26 @@ class TableManager extends Component {
     // adding a new column should add a new field in TRACKED-DATA
   }
 
+  updateSelectedRow(event, coords, TD)
+  {
+    console.log(coords);
+    this.props.setColumn(coords["col"]);
+  }
+
   componentDidMount() {
     console.log(this.props);
     this.loadDataToTable();
     Handsontable.hooks.add('afterChange', (change, source)=>this.updateFirebase(change, source));
+    Handsontable.hooks.add('afterOnCellMouseDown', (event, coords, TD)=>this.updateSelectedRow(event, coords, TD));
     // this.hotTableComponent.current.hotInstance.addHook('afterChange', this.updateFirebase);
   }
 
   updateFirebase(changes, source)
   {
-    console.log(changes);
     if (source === 'loadData') {
         return; //don't save this change
     }
+        console.log(changes);
     console.log(this);
     if (this.props != null){
       for (let i = 0; i < changes.length; i++)
@@ -296,13 +353,14 @@ class TableManager extends Component {
   {
     console.log("LOAD DATA TO TABLE: " + this.props.dataFromFirebase);
     var newData = [];
-    console.log(newData);
-    var colHeaders = this.props.dataFromFirebase[0];
+    console.log(this.props.dataItemNames);
+    console.log(this.props.dataItemNames.indexOf("TRACKED-DATA"));
+    var colHeaders = this.props.dataFromFirebase[this.props.dataItemNames.indexOf("TRACKED-DATA")];
     console.log(colHeaders);
     for (let i = 0; i < this.props.dataFromFirebase.length; i++)
     {
-      if (i === 0) {
-        newData.push(this.props.dataFromFirebase[i]);
+      if (i === this.props.dataItemNames.indexOf("TRACKED-DATA")) {
+        newData.unshift(this.props.dataFromFirebase[i]);
       }
       else {
         var newItem = [];
@@ -310,6 +368,7 @@ class TableManager extends Component {
         {
           newItem[field] = this.props.dataFromFirebase[i][colHeaders[field]];
         }
+        console.log(newItem);
         newData.push(newItem);
       }
     }
@@ -332,10 +391,11 @@ class TableManager extends Component {
     }
     else if (type === "ROW") {
       this.hotTableComponent.current.hotInstance.alter('insert_row', 1);
+      // also add a new entry
+      this.props.addNewEntry();
     }
 
     this.hotTableComponent.current.hotInstance.render();
-    console.log(this.hotTableComponent.current.hotInstance.getData());
   }
 
   render() {
@@ -361,7 +421,7 @@ class TableManager extends Component {
             </button>
           </div>
           <div className="RightDock">
-            <button onClick={() => this.props.addNewEntry()}>
+            <button onClick={() => this.handleAddClick("ROW")}>
               ADD NEW ENTRY
             </button>
           </div>
